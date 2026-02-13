141942a12e588689573cb371cdca937d58d55a2e
omarfarukx11 <khfarukcht@gmail.com>
2026-02-13 16:11:24 +0600

    login api add

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,59 @@
+import { dbConnect } from "@/lib/dbConnect";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        const user = await dbConnect("users").findOne({ email: email });
+
+        if (!user) {
+          throw new Error("No user found");
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatched) {
+          throw new Error("Invalid password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.role = token.role;
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  pages: {
+    signIn: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
